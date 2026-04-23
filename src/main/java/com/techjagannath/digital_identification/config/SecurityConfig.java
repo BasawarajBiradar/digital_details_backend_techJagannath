64e599a9c5126ae4ebea3cfdff679a69f08e5a76
BasawarajBiradar <basawarajbiradar007@gmail.com>
2026-04-23 10:32:17 +0530

    udpated cors config

--- src/main/java/com/techjagannath/digital_identification/config/SecurityConfig.java
+++ src/main/java/com/techjagannath/digital_identification/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200")); // Angular
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://tapaxe.techjagannath.com")); // Angular
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
