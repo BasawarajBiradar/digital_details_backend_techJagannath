f5fe9200e5b335725d6c14cab23939dcbcd614aa
BasawarajBiradar <basawarajbiradar007@gmail.com>
2026-05-29 00:05:45 +0530

    school admin : dashboard : bug fix

--- src/main/java/com/techjagannath/digitalidentification/service/schooladmin/impl/SchoolAdminServiceImpl.java
+++ src/main/java/com/techjagannath/digitalidentification/service/schooladmin/impl/SchoolAdminServiceImpl.java
@@ -99,7 +99,7 @@ public class SchoolAdminServiceImpl implements SchoolAdminService {
             String classLevel = res[4].toString();
             String division = res[5] == null ? null : res[5].toString();
             String registrationDate = res[6].toString().split(" ")[0];
-            String isPresent = res[7].toString();
+            Boolean isPresent = (Boolean) res[7];
 
             StringBuilder fullName = new StringBuilder(firstName);
             if (middleName != null)
